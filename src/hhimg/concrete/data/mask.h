4013540874de1f680d90de93e6ce72a158e6a75e
drfailer <drfailer.42@gmail.com>
2024-05-03 11:35:36 +0200

    mask class (for mask algorithms)

---
+++ src/hhimg/concrete/data/mask.h
@@ -0,0 +1,33 @@
+#ifndef MASK_HPP
+#define MASK_HPP
+#include <cstddef>
+#include <stdexcept>
+#include <vector>
+
+namespace hhimg {
+
+template <typename T> class Mask {
+  public:
+    Mask(std::vector<int> mask, size_t width, size_t height)
+        : mask_(mask), width_(width), height_(height) {
+        if (width % 2 == 0 || height % 2 == 0) {
+            throw std::invalid_argument("error: the width and the height of a "
+                                        "mask must be an odd value.");
+        }
+    }
+    virtual ~Mask() {}
+
+    size_t width() const { return width_; }
+    size_t height() const { return height_; }
+
+    T get(size_t mx, size_t my) const { return mask_[my * width_ + mx]; }
+
+  private:
+    std::vector<int> mask_ = {};
+    size_t width_ = 0;
+    size_t height_ = 0;
+};
+
+} // namespace hhimg
+
+#endif
