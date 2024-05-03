76afc376949668f6d3f3e308691f554bdf1be2b8
drfailer <drfailer.42@gmail.com>
2024-05-03 11:38:41 +0200

    main header of the library

---
+++ src/hhimg/hhimg.h
@@ -0,0 +1,15 @@
+#ifndef HHIMG_HPP
+#define HHIMG_HPP
+
+#include "./abstract/data/abstract_image.h"
+#include "./abstract/data/abstract_pixel.h"
+#include "./abstract/abstract_pixel_factory.h"
+#include "./abstract/abstract_algorithm.h"
+
+#include "./concrete/data/mask.h"
+#include "./concrete/chain_algorithm.h"
+
+#include "./algorithm/mask_applier.h"
+#include "./algorithm/grayscale.h"
+
+#endif
