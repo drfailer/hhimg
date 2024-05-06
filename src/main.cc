c50f53a0e0b4f8d60e74277a587c7c5addd7711b
drfailer <drfailer.42@gmail.com>
2024-05-06 14:49:11 +0200

    cleanup main

--- src/main.cc
+++ src/main.cc
@@ -1,30 +1,53 @@
-#include <hhimg/hhimg.h>
 #include "impl/cimg/cimg_image.h"
 #include <CImg/CImg.h>
+#include <hhimg/hhimg.h>
 
 using PixelType = unsigned char;
 #undef GrayScale
 
-int main(int, char **) {
-    auto image = std::make_shared<CImgImage<PixelType>>("../img/rivendell.jpg");
-    auto secondImage = std::make_shared<CImgImage<PixelType>>("../img/rivendell.jpg");
-    std::vector<double> v(9, 1.0 / 9);
-    hhimg::Mask<double> meanFilter(v, 3, 3);
-    hhimg::Mask<double> verticalBorders({-1, 0, 1}, 3, 1);
-    hhimg::Mask<double> horizontalBorders({-1, 0, 1}, 1, 3);
-
-    secondImage |= hhimg::MaskApplier<PixelType, double>(meanFilter);
-    image |= hhimg::Minus<PixelType>(secondImage);
-    /* image |= hhimg::GrayScale<PixelType>() | */
-    /*          hhimg::MaskApplier<PixelType, double>(verticalBorders) | */
-    /*          hhimg::NonMaximumSuppression<PixelType>(50); */
-
-    /* cimg_library::CImgDisplay main_disp(image.crop(0, 0, 0, 0, image.width()
-     * - 1, image.height() - 1, 0, 1),"Click a point"); */
+void displayCImgImage(std::shared_ptr<CImgImage<PixelType>> image) {
     cimg_library::CImgDisplay main_disp(image->image(), "Click a point");
 
     while (!main_disp.is_closed()) {
         main_disp.wait();
     }
+}
+
+void verticalBordersExtr(std::string const &filename) {
+    auto image = std::make_shared<CImgImage<PixelType>>(filename);
+    hhimg::Mask<double> verticalBorders({-1, 0, 1}, 3, 1);
+
+    image |= hhimg::GrayScale<PixelType>() |
+             hhimg::MaskApplier<PixelType, double>(verticalBorders) |
+             hhimg::NonMaximumSuppression<PixelType>(50);
+    displayCImgImage(image);
+}
+
+void horizontalBordersExtr(std::string const &filename) {
+    auto image = std::make_shared<CImgImage<PixelType>>(filename);
+    hhimg::Mask<double> horizontalBorders({-1, 0, 1}, 1, 3);
+
+    image |= hhimg::GrayScale<PixelType>() |
+             hhimg::MaskApplier<PixelType, double>(horizontalBorders) |
+             hhimg::NonMaximumSuppression<PixelType>(50);
+    displayCImgImage(image);
+}
+
+void detailExtr(std::string const &filename) {
+    auto image = std::make_shared<CImgImage<PixelType>>(filename);
+    auto secondImage = image->copy();
+    std::vector<double> v(9, 1.0 / 9);
+    hhimg::Mask<double> meanFilter(v, 3, 3);
+
+    secondImage |= hhimg::MaskApplier<PixelType, double>(meanFilter);
+    image |= hhimg::Minus<PixelType>(secondImage) |
+             hhimg::NonMaximumSuppression<PixelType>(10);
+    displayCImgImage(image);
+}
+
+int main(int, char **) {
+    /* verticalBordersExtr("../img/rivendell.jpg"); */
+    /* horizontalBordersExtr("../img/rivendell.jpg"); */
+    detailExtr("../img/rivendell.jpg");
     return 0;
 }
