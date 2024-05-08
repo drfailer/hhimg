03b041827fbf548897f502a8caf21c56c8f6dde8
drfailer <drfailer.42@gmail.com>
2024-05-08 14:50:02 +0200

    added more utility functions for the pixel

--- src/hhimg/concrete/data/pixel.h
+++ src/hhimg/concrete/data/pixel.h
@@ -1,6 +1,7 @@
 #ifndef PIXEL_HPP
 #define PIXEL_HPP
 #include "../../tools/utils.h"
+#include <ostream>
 
 namespace hhimg {
 
@@ -10,8 +11,12 @@ template <typename T> struct Pixel {
     T blue = 0;
 };
 
+template <typename T> Pixel<T> pixel(T r, T g, T b) { return {r, g, b}; }
+
+template <typename T> Pixel<T> pixel(T v) { return {v, v, v}; }
+
 template <typename Out, typename In>
-Pixel<Out> validate(Pixel<In> const &pixel) {
+Pixel<Out> pixelValidCast(Pixel<In> const &pixel) {
     return {(Out)utils::validate(pixel.red), (Out)utils::validate(pixel.green),
             (Out)utils::validate(pixel.blue)};
 }
@@ -24,6 +29,13 @@ Pixel<Out> pixelCast(Pixel<In> const &pixel) {
 template <typename T>
 bool operator==(Pixel<T> const &lhs, Pixel<T> const &rhs) {
     return lhs.red == rhs.red && lhs.green == rhs.green && lhs.blue == rhs.blue;
+}
+
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const Pixel<T> &pixel) {
+    os << "{ " << +pixel.red << ", " << +pixel.green << ", " << +pixel.blue
+       << " }";
+    return os;
 }
 
 #define PixelOpPixel(Op)                                                       \
@@ -39,6 +51,10 @@ PixelOpPixel(+) PixelOpPixel(-) PixelOpPixel(/) PixelOpPixel(*)
 #define PixelOpValue(Op)                                                       \
     template <typename T, typename RhsT>                                       \
     Pixel<RhsT> operator Op(Pixel<T> const &lhs, RhsT rhs) {                   \
+        return {lhs.red Op rhs, lhs.green Op rhs, lhs.blue Op rhs};            \
+    }                                                                          \
+    template <typename RhsT, typename T>                                       \
+    Pixel<RhsT> operator Op(RhsT rhs, Pixel<T> const &lhs) {                   \
         return {lhs.red Op rhs, lhs.green Op rhs, lhs.blue Op rhs};            \
     }
     PixelOpValue(+) PixelOpValue(-) PixelOpValue(/) PixelOpValue(*)
@@ -57,7 +73,7 @@ PixelOpPixel(+) PixelOpPixel(-) PixelOpPixel(/) PixelOpPixel(*)
 
 #define PixelOpEqValue(Op)                                                     \
     template <typename T, typename RhsT>                                       \
-    const Pixel<T> &operator Op##=(Pixel<T> &lhs, RhsT const rhs) {            \
+    Pixel<T> const &operator Op##=(Pixel<T> &lhs, RhsT const rhs) {            \
         lhs.red Op## = rhs;                                                    \
         lhs.green Op## = rhs;                                                  \
         lhs.blue Op## = rhs;                                                   \
