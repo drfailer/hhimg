57e3049cf2dd541b1f736058c9a6c1a3813c415f
drfailer <drfailer.42@gmail.com>
2024-05-08 14:50:07 +0200

    test on pixel

--- tests/hhimg_test.cc
+++ tests/hhimg_test.cc
@@ -7,6 +7,50 @@
 
 using PixelType = unsigned char;
 
+TEST(Pixels, Operators) {
+    hhimg::Pixel<PixelType> p1 = {1, 2, 3};
+    hhimg::Pixel<PixelType> p2 = {10, 20, 30};
+    hhimg::Pixel<PixelType> p3 = {10, 10, 10};
+
+    // pixel Op pixel
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p1 + p2), hhimg::pixel<PixelType>(11, 22, 33));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p2 - p1), hhimg::pixel<PixelType>(9, 18, 27));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p2 - p2), hhimg::pixel<PixelType>(0));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p2 * p1), hhimg::pixel<PixelType>(10, 40, 90));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p1 * p1), hhimg::pixel<PixelType>(1, 4, 9));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p2 / p2), hhimg::pixel<PixelType>(1));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p2 / p1), hhimg::pixel<PixelType>(10));
+
+    // with values
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p1 + 4), hhimg::pixel<PixelType>(5, 6, 7));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p2 - 4), hhimg::pixel<PixelType>(6, 16, 26));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p3 * 4), hhimg::pixel<PixelType>(40));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p3 / 10), hhimg::pixel<PixelType>(1));
+
+    // multimple operations
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p2 / p1 + p3), hhimg::pixel<PixelType>(20));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(p2 / p1 * p3 - p2), hhimg::pixel<PixelType>(90, 80, 70));
+    ASSERT_EQ(hhimg::pixelCast<PixelType>(1 + p2 / p1 * 10), hhimg::pixel<PixelType>(101));
+}
+
+TEST(Pixels, AffactionOperators) {
+    hhimg::Pixel<PixelType> p1 = {1, 2, 3};
+    hhimg::Pixel<PixelType> p2 = {10, 20, 30};
+    hhimg::Pixel<PixelType> p3 = {10, 10, 10};
+
+    // pixel Op pixel
+    p1 += p2;
+    p2 -= p3 - (p1 - p2);
+    p3 *= 4;
+    p3 /= 10;
+
+    std::cout << p2 << std::endl;
+
+    ASSERT_EQ(p1, hhimg::pixel<PixelType>(11, 22, 33));
+    ASSERT_EQ(p2, hhimg::pixel<PixelType>(1, 12, 23));
+    ASSERT_EQ(p3, hhimg::pixel<PixelType>(4));
+}
+
 TEST(Images, ReadAccessor) {
     constexpr size_t width = 10;
     constexpr size_t height = 10;
@@ -39,11 +83,15 @@ TEST(Images, WriteAccessors) {
     // modifications
     image.set(0, 0, 10, 20, 30);
     image.set(9, 9, 20, 40, 60);
+    image.set(1, 0, 4);
     image.set(3, 5, 1, 2, 3);
+    image.set(0, 1, {12, 13, 14});
 
     ASSERT_EQ(mem[0], RGBValue<PixelType>(10, 20, 30));
     ASSERT_EQ(mem[9 * width + 9], RGBValue<PixelType>(20, 40, 60));
+    ASSERT_EQ(mem[1], RGBValue<PixelType>(4, 4, 4));
     ASSERT_EQ(mem[5 * width + 3], RGBValue<PixelType>(1, 2, 3));
+    ASSERT_EQ(mem[1 * width], RGBValue<PixelType>(12, 13, 14));
 
     // free memory
     delete[] mem;
