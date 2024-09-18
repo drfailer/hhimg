b1549c7215a29d5bff2f284a9ab0e787f2d24cd6
drfailer <drfailer.42@gmail.com>
2024-09-18 12:58:29 -0400

    Added a make pair algorithm that will be used to create the pairs of tiles for the pair tile algorithms.

---
+++ src/hhimg/algorithm/tile/make_pair.h
@@ -0,0 +1,35 @@
+#ifndef MAKE_PAIR_H
+#define MAKE_PAIR_H
+#include "../../abstract/abstract_tile_algorithm.h"
+#include "../../abstract/abstract_tile_factory.h"
+#include "../../concrete/data/pixel_tile.h"
+#include <hedgehog/hedgehog.h>
+
+namespace hhimg {
+
+template <typename T>
+using MakePairTaskType =
+    hh::AbstractTask<1, AbstractTile<T>, std::pair<Tile<T>, Tile<T>>>;
+
+template <typename T> struct MakePair : MakePairTaskType<T> {
+    MakePair(size_t nbThreads) : MakePairTaskType<T>("MakePair", nbThreads) {}
+    ~MakePair() = default;
+
+    void execute(Tile<T> tile) override {
+        this->addResult(std::make_shared<std::pair<Tile<T>, Tile<T>>>(
+            std::move(tile), std::make_shared<PixelTile<T>>(
+                                 tile->x(), tile->y(), tile->tileSize(),
+                                 tile->ghostRegionSize(), tile->image())));
+    }
+
+    std::shared_ptr<MakePairTaskType<T>> copy() override {
+        return std::make_shared<MakePair<T>>(this->numberThreads());
+    }
+
+    std::shared_ptr<AbstractTileFactory<T>> tileFactory = nullptr;
+    size_t tileSize = 0;
+};
+
+} // end namespace hhimg
+
+#endif
