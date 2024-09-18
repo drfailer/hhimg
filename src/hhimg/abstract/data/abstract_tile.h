03af7d5bbce4801db67743a9f0ba7a97dc2a9c7b
drfailer <drfailer.42@gmail.com>
2024-09-18 12:55:37 -0400

    fix some issues in abstract tile + reformat and remove useless code

--- src/hhimg/abstract/data/abstract_tile.h
+++ src/hhimg/abstract/data/abstract_tile.h
@@ -2,30 +2,27 @@
 #define ABSTRACT_TILE_HPP
 #include "abstract_image.h"
 #include "abstract_pixel_container.h"
-#include <functional>
 #include <memory>
 
 namespace hhimg {
 
 template <typename T> class AbstractTile : public AbstractPixelContainer<T> {
   public:
-    AbstractTile(size_t x, size_t y, size_t tileSize, size_t ghostTileSize,
+    AbstractTile(size_t x, size_t y, size_t tileSize, size_t ghostRegionSize,
                  std::shared_ptr<AbstractImage<T>> image)
         : x_(x), y_(y), width_(std::min(tileSize, image->width() - x)),
           height_(std::min(tileSize, image->height() - y)), tileSize_(tileSize),
-          ghostX_(std::max((int)(x - tileSize), 0)),
-          ghostY_(std::max((int)(y - tileSize), 0)),
+          ghostTileSize_(tileSize + 2 * ghostRegionSize),
+          ghostX_(std::max((int)(x - ghostRegionSize), 0)),
+          ghostY_(std::max((int)(y - ghostRegionSize), 0)),
           ghostWidth_(std::min(ghostTileSize_, image->width() - ghostX_)),
           ghostHeight_(std::min(ghostTileSize_, image->height() - ghostY_)),
-          ghostTileSize_(ghostTileSize) {}
-    AbstractTile(size_t x, size_t y, size_t tileSize,
-                 std::shared_ptr<AbstractImage<T>> image)
-        : x_(x), y_(y), width_(std::min(tileSize, image->width() - x)),
-          height_(std::min(tileSize, image->height() - y)),
-          tileSize_(tileSize) {}
+          image_(image) {}
 
     // get the full image
-    virtual std::shared_ptr<AbstractImage<T>> image() const = 0;
+    virtual std::shared_ptr<AbstractImage<T>> const image() const {
+      return image_;
+    }
 
     // tile position in the image
     size_t x() const { return x_; }
@@ -33,12 +30,30 @@ template <typename T> class AbstractTile : public AbstractPixelContainer<T> {
 
     // tile size
     size_t width() const override { return width_; }
-    size_t fullWidth() const override { return this->image()->width(); }
     size_t height() const override { return height_; }
     size_t tileSize() const { return tileSize_; }
 
     using AbstractPixelContainer<T>::set;
     using AbstractPixelContainer<T>::at;
+
+    Pixel<T> at(size_t x, size_t y) const override {
+        if (ghostY_ > 0) {
+          y += ghostRegionSize();
+        }
+        if (ghostX_ > 0) {
+          x += ghostRegionSize();
+        }
+        return ghostAt(x, y);
+    }
+    void set(size_t x, size_t y, Pixel<T> const &pixel) override {
+        if (ghostY_ > 0) {
+          y += ghostRegionSize();
+        }
+        if (ghostX_ > 0) {
+          x += ghostRegionSize();
+        }
+        ghostSet(x, y, pixel);
+    }
 
     /* ghost tile *************************************************************/
 
@@ -49,28 +64,28 @@ template <typename T> class AbstractTile : public AbstractPixelContainer<T> {
     size_t ghostTileSize() const { return ghostTileSize_; }
     size_t ghostRegionSize() const { return (ghostTileSize_ - tileSize_) / 2; }
 
-    virtual Pixel<T> ghostAt(size_t) const { throw std::bad_function_call(); }
-    virtual void ghostSet(size_t, Pixel<T> const &) { throw std::bad_function_call(); }
+    virtual Pixel<T> ghostAt(size_t) const = 0;
+    virtual void ghostSet(size_t, Pixel<T> const &) = 0;
 
     virtual Pixel<T> ghostAt(size_t x, size_t y) const {
-        return ghostAt(y * fullWidth() + x);
+        return ghostAt(y * this->fullWidth() + x);
     }
     virtual void ghostSet(size_t x, size_t y, Pixel<T> const &pixel) {
-        ghostSet(y * fullWidth() + x, pixel);
+        ghostSet(y * this->fullWidth() + x, pixel);
     }
 
     virtual void ghostSet(size_t offset, T r, T g, T b) {
-        set(offset, {r, g, b});
+        ghostSet(offset, {r, g, b});
     }
     virtual void ghostSet(size_t x, size_t y, T r, T g, T b) {
-        ghostSet(y * fullWidth() + x, r, g, b);
+        ghostSet(y * this->fullWidth() + x, r, g, b);
     }
 
     // for grayscaled images
-    void ghostSet(size_t offset, T v) { set(offset, v, v, v); }
-    void ghostSet(size_t x, size_t y, T v) { set(x, y, v, v, v); }
-    T ghostGet(size_t offset) const { return at(offset).red; }
-    T ghostGet(size_t x, size_t y) const { return at(x, y).red; }
+    void ghostSet(size_t offset, T v) { ghostSet(offset, v, v, v); }
+    void ghostSet(size_t x, size_t y, T v) { ghostSet(x, y, v, v, v); }
+    T ghostGet(size_t offset) const { return ghostAt(offset).red; }
+    T ghostGet(size_t x, size_t y) const { return ghostAt(x, y).red; }
 
   protected:
     size_t x_ = 0;
@@ -80,11 +95,14 @@ template <typename T> class AbstractTile : public AbstractPixelContainer<T> {
     size_t tileSize_ = 0;
 
     // ghost region
+    size_t ghostTileSize_ = 0;
     size_t ghostX_ = 0;
     size_t ghostY_ = 0;
     size_t ghostWidth_ = 0;
     size_t ghostHeight_ = 0;
-    size_t ghostTileSize_ = 0;
+
+    // the image
+    std::shared_ptr<AbstractImage<T>> image_ = nullptr;
 };
 
 } // namespace hhimg
