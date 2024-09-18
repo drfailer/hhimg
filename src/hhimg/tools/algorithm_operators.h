870aae22a19ef596cafa1e99841720e52c59535d
drfailer <drfailer.42@gmail.com>
2024-09-18 13:02:45 -0400

    update the operators with the new algorithms kind

--- src/hhimg/tools/algorithm_operators.h
+++ src/hhimg/tools/algorithm_operators.h
@@ -1,11 +1,16 @@
 #ifndef ALGORITHM_OPERATORS_HPP
 #define ALGORITHM_OPERATORS_HPP
 #include "../abstract/abstract_algorithm.h"
-#include "../abstract/abstract_tile_algorithm.h"
 #include "../abstract/abstract_hh_algorithm.h"
-#include "../concrete/chain_algorithm.h"
+#include "../abstract/abstract_pair_tile_algorithm.h"
+#include "../abstract/abstract_tile_algorithm.h"
+#include "../algorithm/tile/ghost_region_state.h"
+#include "../algorithm/tile/make_pair.h"
 #include "../algorithm/tile/split.h"
+#include "../concrete/chain_algorithm.h"
+#include "hhimg/algorithm/tile/wire.h"
 #include <memory>
+#include <type_traits>
 
 /******************************************************************************/
 /*                                   images                                   */
@@ -34,13 +39,16 @@ template <typename Img, typename T>
 std::shared_ptr<Img>
 operator|=(std::shared_ptr<Img> image,
            std::shared_ptr<hhimg::AbstractHHAlgorithm<T>> algorithm) {
-  return std::dynamic_pointer_cast<Img>(algorithm->operator()(image));
+    return std::dynamic_pointer_cast<Img>(algorithm->operator()(image));
 }
 
 template <typename T>
 std::shared_ptr<hhimg::AbstractHHAlgorithm<T>>
 operator|(std::shared_ptr<hhimg::Split<T>> split,
           std::shared_ptr<hhimg::AbstractTileAlgorithm<T>> algorithm) {
+    if (split->ghostRegionSize() < algorithm->ghostRegionSize()) {
+        split->ghostRegionSize(algorithm->ghostRegionSize());
+    }
     split->push_back(algorithm);
     return split;
 }
@@ -49,15 +57,38 @@ template <typename T>
 std::shared_ptr<hhimg::AbstractHHAlgorithm<T>>
 operator|(std::shared_ptr<hhimg::AbstractHHAlgorithm<T>> hhAlgo,
           std::shared_ptr<hhimg::AbstractTileAlgorithm<T>> algorithm) {
+    if (hhAlgo->ghostRegionSize() < algorithm->ghostRegionSize()) {
+        hhAlgo->ghostRegionSize(algorithm->ghostRegionSize());
+    }
     hhAlgo->push_back(algorithm);
     return hhAlgo;
 }
 
 template <typename T>
 std::shared_ptr<hhimg::AbstractHHAlgorithm<T>>
-operator|(std::shared_ptr<hhimg::AbstractTileAlgorithm<T>> algorithm,
-          std::shared_ptr<hhimg::AbstractHHAlgorithm<T>> hhAlgo) {
-    hhAlgo->push_front(algorithm);
+operator|(std::shared_ptr<hhimg::AbstractHHAlgorithm<T>> hhAlgo,
+          std::shared_ptr<hhimg::AbstractPairTileAlgorithm<T>> algorithm) {
+    if (hhAlgo->ghostRegionSize() < algorithm->ghostRegionSize()) {
+        hhAlgo->ghostRegionSize(algorithm->ghostRegionSize());
+    }
+    auto makePairTask =
+        std::make_shared<hhimg::MakePair<T>>(algorithm->numberThreads());
+    auto ghostRegionState = std::make_shared<hhimg::SyncGhostRegions<T>>();
+    auto ghostRegionsStateManager = std::make_shared<
+        hh::StateManager<1, std::pair<hhimg::Tile<T>, hhimg::Tile<T>>,
+                         std::pair<hhimg::Tile<T>, hhimg::Tile<T>>>>(
+        ghostRegionState);
+    auto wire = std::make_shared<hhimg::Wire<T>>(algorithm->numberThreads());
+
+    if (!hhAlgo->lastTask()) {
+      hhAlgo->inputs(makePairTask);
+    } else {
+      hhAlgo->edges(hhAlgo->lastTask(), makePairTask);
+    }
+    hhAlgo->edges(makePairTask, ghostRegionsStateManager);
+    hhAlgo->edges(ghostRegionsStateManager, algorithm);
+    hhAlgo->edges(algorithm, wire);
+    hhAlgo->lastTask(wire);
     return hhAlgo;
 }
 
