950507a5ed6552c63d5fb2261417368f2c3d9e1e
drfailer <drfailer.42@gmail.com>
2024-09-19 18:31:16 -0400

    fix: wrong offset in the cimg tile factory

--- src/impl/cimg/cimg_tile_factory.cc
+++ src/impl/cimg/cimg_tile_factory.cc
@@ -6,8 +6,7 @@ std::shared_ptr<hhimg::PixelTile<T>>
 CImgTileFactory<T>::get(size_t x, size_t y, size_t ghostRegionSize) const {
     auto tile = std::make_shared<hhimg::PixelTile<T>>(x, y, this->tileSize_,
                                                       ghostRegionSize, image_);
-
-    size_t offset = tile->ghostY() * tile->fullWidth() + tile->ghostX();
+    size_t offset = tile->ghostY() * image_->width() + tile->ghostX();
     size_t size = image_->width() * image_->height() * image_->image().depth();
     auto dataRed = &image_->image()._data[offset];
     auto dataGreen = &image_->image()._data[offset + size];
