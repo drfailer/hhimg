e1168351de2d1be27436be1269cde96350b4a3fb
drfailer <drfailer.42@gmail.com>
2024-09-20 11:00:51 -0400

    fix: wrong pointers in the cimg_tile_factory
    note: their might be issues with the at and set methods as well

--- src/impl/cimg/cimg_tile_factory.cc
+++ src/impl/cimg/cimg_tile_factory.cc
@@ -6,11 +6,12 @@ std::shared_ptr<hhimg::PixelTile<T>>
 CImgTileFactory<T>::get(size_t x, size_t y, size_t ghostRegionSize) const {
     auto tile = std::make_shared<hhimg::PixelTile<T>>(x, y, this->tileSize_,
                                                       ghostRegionSize, image_);
-    size_t offset = tile->ghostY() * image_->width() + tile->ghostX();
-    size_t size = image_->width() * image_->height() * image_->image().depth();
-    auto dataRed = &image_->image()._data[offset];
-    auto dataGreen = &image_->image()._data[offset + size];
-    auto dataBlue = &image_->image()._data[offset + 2 * size];
+    auto dataRed =
+        &image_->image().atXYZC(tile->ghostX(), tile->ghostY(), 0, 0);
+    auto dataGreen =
+        &image_->image().atXYZC(tile->ghostX(), tile->ghostY(), 0, 1);
+    auto dataBlue =
+        &image_->image().atXYZC(tile->ghostX(), tile->ghostY(), 0, 2);
 
     for (size_t iy = 0; iy < tile->ghostHeight(); ++iy) {
         for (size_t ix = 0; ix < tile->ghostWidth(); ++ix) {
