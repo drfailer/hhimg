588cfc565eb691247e76d568a5b4690ed9f9975a
drfailer <drfailer.42@gmail.com>
2024-09-20 14:38:52 -0400

    new hh and tile algorithms -> avoid code duplication + hh algorithm should be abstract

---
+++ src/hhimg/abstract/hh/abstract_pair_tile_algorithm.h
@@ -0,0 +1,22 @@
+#ifndef ABSTRACT_PAIR_TILE_ALGORITHM_H
+#define ABSTRACT_PAIR_TILE_ALGORITHM_H
+#include "../data/abstract_tile.h"
+#include "abstract_hh_algorithm.h"
+
+namespace hhimg {
+
+template <typename T>
+using PairTile = std::pair<std::shared_ptr<AbstractTile<T>>, std::shared_ptr<AbstractTile<T>>>;
+
+template <typename T>
+struct AbstractPairTileAlgorithm
+    : AbstractHHAlgorithm<PairTile<T>, AbstractTile<T>> {
+    using AbstractHHAlgorithm<PairTile<T>,
+                              AbstractTile<T>>::AbstractHHAlgorithm;
+    using TaskType =
+        typename AbstractHHAlgorithm<PairTile<T>, AbstractTile<T>>::TaskType;
+};
+
+} // namespace hhimg
+
+#endif
---
+++ src/hhimg/abstract/hh/abstract_tile_algorithm.h
@@ -0,0 +1,18 @@
+#ifndef ABSTRACT_TILE_ALGORITHM_H
+#define ABSTRACT_TILE_ALGORITHM_H
+#include "../data/abstract_tile.h"
+#include "abstract_hh_algorithm.h"
+
+namespace hhimg {
+
+template <typename T>
+struct AbstractTileAlgorithm
+    : AbstractHHAlgorithm<AbstractTile<T>, AbstractTile<T>> {
+    using AbstractHHAlgorithm<AbstractTile<T>,
+                              AbstractTile<T>>::AbstractHHAlgorithm;
+    using TaskType = typename AbstractHHAlgorithm<AbstractTile<T>, AbstractTile<T>>::TaskType;
+};
+
+} // namespace hhimg
+
+#endif
