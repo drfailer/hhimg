d0c06b3f3bcab92f04ca88ca8d856ec32ca1b72c
drfailer <drfailer.42@gmail.com>
2024-09-30 14:46:47 -0400

    fast loader algorithm

---
+++ src/hhimg/abstract/fast_loader/abstract_fl_algorithm.h
@@ -0,0 +1,27 @@
+#ifndef ABSTRACT_FL_ALGORITHM_H
+#define ABSTRACT_FL_ALGORITHM_H
+#include <hedgehog/hedgehog.h>
+
+namespace hhimg {
+
+template <typename Input, typename Output>
+struct AbstractFLAlgorithm : hh::AbstractTask<1, Input, Output> {
+    AbstractFLAlgorithm(size_t nbThreads = 1,
+                        std::string const &name = "Unnamed Algorithm")
+        : hh::AbstractTask<1, Input, Output>(name, nbThreads) {}
+
+    void execute(std::shared_ptr<Input> in) override { this->operator()(in); }
+
+    // can be overriden if a ghost region is required
+    virtual size_t ghostRegionSize() const { return 0; }
+
+    virtual void operator()(std::shared_ptr<Input> in) = 0;
+
+    using InputType = Input;
+    using OutputType = Output;
+    using TaskType = hh::AbstractTask<1, Input, Output>;
+};
+
+} // end namespace hhimg
+
+#endif
