965792c3c319daafe74a344efe6e06eaf2be346c
drfailer <drfailer.42@gmail.com>
2024-10-04 10:07:37 -0400

    map mutate algorithm for fast loader (not tested)

--- src/hhimg/fast_loader/algorithm/map_mutate.h
+++ src/hhimg/fast_loader/algorithm/map_mutate.h
@@ -1,11 +1,38 @@
 #ifndef FLD_MAP_MUTATE_HPP
 #define FLD_MAP_MUTATE_HPP
+#include "../../tools/utils.h"
+#include "../abstract/view_algorithms.h"
+#include "../concrete/data/fl_view.h"
+#include <functional>
 
 namespace hhimg::fld {
 
-template <typename T>
-struct MapMutate {
-  // TODO
+template <typename T> struct MapMutate : AbstractViewAlgorithm<T> {
+    using ComputeFn = std::function<T(std::shared_ptr<View<T>>, size_t idx)>;
+
+    MapMutate(size_t nbThreads, ComputeFn compute)
+        : AbstractViewAlgorithm<T>(nbThreads, "MapMutate"), compute_(compute) {}
+
+    void operator()(std::shared_ptr<View<T>> view) override {
+        size_t size = utils::computeSize(view->tileDims());
+
+        for (size_t i = 0; i < size; ++i) {
+            view->viewData()->data()[i] = compute_(view->viewData()->data(), i);
+        }
+        this->addResult(view);
+    }
+
+    std::shared_ptr<typename AbstractViewAlgorithm<T>::TaskType>
+    copy() override {
+        return std::make_shared<MapMutate<T>>(this->numberThreads(), compute_);
+    }
+
+    constexpr static auto setup(auto pipeline, auto self) {
+        return pipeline->add(self);
+    }
+
+  private:
+    ComputeFn compute_;
 };
 
 } // namespace hhimg::fld
